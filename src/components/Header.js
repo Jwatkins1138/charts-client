b620c8009c32dfe80e6b1ac7cd868d4e4eb4cd1a
Jason Watkins <goushoryuken@gmail.com>
2022-08-20 14:44:39 -0700

    dns

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <header>
-      <h1>mf charts</h1>
+      <h1>allcharts.site</h1>
+      <p>animate-aptosaurus-gvt2g77oilqfwzplnv83oprd.herokudns.com.
+      </p>
     </header>
   )
 }

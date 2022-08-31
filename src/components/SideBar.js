22d8590ef209519346217ea593b679c530ab419e
Jason Watkins <goushoryuken@gmail.com>
2022-08-31 12:32:01 -0700

    sidebar initial functions

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,11 +1,54 @@
+import { useState, useEffect, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { currentUser } from '../helpers'
+import AuthContext from '../context/AuthProvider'
+
 const SideBar = () => {
+
+  const { auth } = useContext(AuthContext);
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.token) {
+      currentUser().then((res) => {
+        setUser(res);
+      });
+    } else {
+      setUser({});
+    }
+  }, [auth]);
+
+  const clickItem = (e) => {
+    console.log(e.target.id);
+    navigate('/chart/' + e.target.id);
+  };
+
+
   return (
     <aside>
-      <div className='side-title'><h5>left nav</h5></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
+      <>
+      { (user && user.id) ? (
+        <>
+        <div className='side-title'><h4>your watch lists</h4></div>
+        
+        </>
+      ) : (
+        <>
+        <div className='side-title'><h4>popular stocks</h4></div>
+        <div className='side-title'><h5>indices/ETFs</h5></div>
+        <div id="SPY" className="side-item" onClick={clickItem}><span>SPY</span></div>
+        <div id="QQQ" className="side-item" onClick={clickItem}><span>QQQ</span></div>
+        <div id="DIA"className="side-item" onClick={clickItem}><span>DIA</span></div>
+        <div id="XLF"className="side-item" onClick={clickItem}><span>XLF</span></div>
+        <div className='side-title'><h5>tech</h5></div>
+        <div id="APP"className="side-item" onClick={clickItem}><span>APP</span></div>
+        <div id="GOOG"className="side-item" onClick={clickItem}><span>GOOG</span></div>
+        <div id="META"className="side-item" onClick={clickItem}><span>META</span></div>
+        <div id="IBM"className="side-item" onClick={clickItem}><span>IBM</span></div>
+        </>
+      )}
+      </>
+      
     </aside>
   )
 }
--- src/components/SideBarRight.js
+++ src/components/SideBarRight.js
@@ -1,11 +1,12 @@
 const SideBarRight = () => {
   return (
     <aside>
-      <div className='side-title'><h5>right nav</h5></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
-      <div className='side-item'><h5>hello from the side</h5><span>thing</span></div>
+      <div className='side-title'><h5>chart options</h5></div>
+      <div className='side-item'><span>thing</span></div>
+      <div className='side-item'><span>thing</span></div>
+      <div className='side-item'><span>thing</span></div>
+      <div className='side-item'><span>thing</span></div>
+      <div className='side-title'><h5>other options</h5></div>
       
     </aside>
   )

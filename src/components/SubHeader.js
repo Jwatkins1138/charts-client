eb278d9ce17419c2993495b7ce85e010df5dc326
Jason Watkins <goushoryuken@gmail.com>
2022-09-02 10:07:42 -0700

    minor posts style

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -5,6 +5,7 @@ const SubHeader = () => {
     <nav className='head-nav'>
       <Link to='/login'><div className='nav-item'><h5>login</h5></div></Link>
       <Link to='/signup'><div className='nav-item'><h5>sign up</h5></div></Link>
+      <Link to='/profile'><div className='nav-item'><h5>profile</h5></div></Link>
       <Link to='/about'><div className='nav-item'><h5>about</h5></div></Link>
     </nav>
   )

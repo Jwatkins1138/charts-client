c7e2085205418ab5abb34763674bb9d057078aa6
Jason Watkins <goushoryuken@gmail.com>
2022-09-02 10:14:39 -0700

    chart link footer

--- src/components/ChartFooter.js
+++ src/components/ChartFooter.js
@@ -4,13 +4,18 @@ const ChartFooter = (props) => {
   const navigate = useNavigate();
   const postsLink = () => {
     navigate('/posts/' + props.ticker)
-  }
+  };
+  const chartLink = () => {
+    navigate('/chart/' + props.ticker)
+  };
+
   return (
     <footer>
       <nav className='head-nav'>
         <div className='nav-item'><h5>company info</h5></div>
         <div onClick={postsLink} className='nav-item'><h5>discussion</h5></div>
         <div className='nav-item'><h5>add to list</h5></div>
+        <div onClick={chartLink} className='nav-item'><h5>chart</h5></div>
       </nav>
     </footer>
   )

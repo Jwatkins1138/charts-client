14dd278dc2f56eddcfc0b80ca9584a28042f3861
Jason Watkins <goushoryuken@gmail.com>
2022-09-02 11:35:54 -0700

    info functions and style

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,25 @@
-const About = () => {
+import Header from './Header'
+import SideBar from './SideBar'
+import SideBarRight from './SideBarRight'
 
+const About = () => {
+  return (
+    <div className='container'>
+      <Header />
+      <main className='about'>
+        <SideBar />
+        <div className='about-main'>
+          <div className='about-card'>
+            <h1>about this site</h1>
+            <div className='about-body'>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+        <SideBarRight />
+      </main>
+    </div>    
+  ) 
 }
 
 export default About;
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,69 @@
+import {useState, useEffect, useContext} from 'react'
+import Header from './Header'
+import SideBar from './SideBar'
+import SideBarRight from './SideBarRight'
+import ChartFooter from './ChartFooter'
+import Loading from './Loading'
+import { useParams } from 'react-router-dom'
+import { alpha } from '../api/axios'
+import AuthContext from '../context/AuthProvider'
+
 const Info = () => {
 
+  const [info, setInfo] = useState({});
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  const { auth } = useContext(AuthContext);
+
+  const key = 'FVE7LEZWLKOMWHDH';
+  const INFO_URL = `/query?function=OVERVIEW&symbol=${params.ticker}&apikey=${key}`;
+
+  const getInfo = () => {
+    alpha.get(INFO_URL)
+    .then((response) => {
+      const tempInfo = [];
+      console.log(response);
+      for (const item in response.data){
+        tempInfo.push([item, response.data[item]]);
+      }
+      setInfo(tempInfo);
+    })
+    .then(() => {
+      setLoading(false);
+    })
+  }
+
+  useEffect(() => {
+    getInfo();
+  }, [])
+
+
+  return (
+    <>
+    {(loading) ? (
+      <Loading />
+    ) : (
+      <>
+    <div className='container'>
+      <Header />
+      <main className='info'>
+        <SideBar />
+        <div className='info-main'>
+          <header className='posts-header'><h4>{params.ticker} company info</h4></header>
+          <div className='info-area'>
+          {info.map((item) => {
+            return <div className='info-item'><b>{item[0]}</b><p>{item[1]}</p></div>
+          })}
+          </div>
+          <ChartFooter />
+        </div>
+        <SideBarRight />
+      </main>
+    </div>
+    </>
+    )}
+    </>
+  )
 }
 
 export default Info;

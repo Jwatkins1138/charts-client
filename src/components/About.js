dc7ab80afafbf885907063e24a4312f653f82122
Jason Watkins <goushoryuken@gmail.com>
2022-09-04 20:51:16 -0700

    about

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,8 @@ const About = () => {
           <div className='about-card'>
             <h1>about this site</h1>
             <div className='about-body'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p>2022 Jason Christopher Watkins</p>
+              <p>contact me: watkinsjasonc@gmail.com</p>
             </div>
           </div>
         </div>

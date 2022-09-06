34c6463bb7f7f594354c4f6d4775bdd6674ff038
Jason Watkins <goushoryuken@gmail.com>
2022-09-06 13:57:14 -0700

    auto suggest

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,16 @@ import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { useState, useEffect, useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { currentUser } from '../helpers'
+import { main } from '../api/axios'
 import SubHeader from './SubHeader'
 import AuthContext from '../context/AuthProvider'
 
 const Header = () => {
   const { auth, setAuth } = useContext(AuthContext);
   const [ticker, setTicker] = useState('');
+  const [tickers, setTickers] = useState([]);
   const [at, setAt] = useState(false);
+  const [drop, setDrop] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,6 +30,44 @@ const Header = () => {
       })
     } 
   }, []);
+
+  useEffect(() => {
+    if (ticker != '') {
+      const TICKER_URL = `/tickers/${ticker}`;
+      main.get(
+        TICKER_URL,
+        {
+          headers: {'Authorization': localStorage.token,
+                    'Content-Type': 'application/json'},
+          withCredentials: false,
+        }
+      )
+      .then(response => {
+      console.log(response);
+      setTickers(response.data);
+      })
+      .catch(err => {
+      console.log(err);
+    })
+    }
+}, [ticker])
+
+  const toggleDrop = () => {
+    return setTimeout(() => {
+      drop ? (setDrop(false)) : (setDrop(true));
+    }, 500);
+  }
+
+  const selectTicker = (e) => {
+    setTicker(e.target.id);
+    console.log(e.target.id);
+  }
+
+  const drawDropDown = (symbol) => {
+      return (
+        <div key={symbol.id} onClick={selectTicker} id={symbol.name} className='drop-symbol'>{symbol.name}</div>
+      )
+  }
 
   useEffect(() => {
     at ? (setAt(false)) : (setAt(at));
@@ -45,13 +86,25 @@ const Header = () => {
       <label htmlFor="search">
         ticker: 
       </label>
-      <input
-        type='text'
-        id='search'
-        placeholder='search'
-        onChange={(e) => {setTicker(e.target.value)}}
-        value={ticker}
-      />
+      <div className='input-container'>
+        <input
+          type='text'
+          id='search'
+          placeholder='search'
+          autoComplete='off'
+          onFocus={toggleDrop}
+          onBlur={toggleDrop}
+          onChange={(e) => {setTicker(e.target.value)}}
+          value={ticker}
+        />
+        { drop ? (
+          <div className='drop-down'>
+            {tickers.map((item) => {
+              return drawDropDown(item);
+            })}
+          </div>
+        ) : (<></>)}
+      </div>
       <button onClick={searchTicker}><FontAwesomeIcon icon={faMagnifyingGlass} /></button>
       </div>
       <nav className='header-side'>
